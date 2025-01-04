917ea97b1f968a689cdd1ec9fcc13807729f571e
danidrd <danielecaliandro.dev@gmail.com>
2025-01-04 22:49:44 +0100

    Fixed dependencies Added react-helmet-async

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import Home from "./pages/Home";
 import Contacts from "./pages/Contact";
 import About from "./pages/About";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <HelmetProvider>
+      <Router
+        basename={
+          process.env.NODE_ENV === "production" ? "/danidrd.github.io" : "/"
+        }
+      >
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contacts</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
   );
 }
 
