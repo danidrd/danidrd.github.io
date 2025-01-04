48e5c50a297e11cf90b1854c9d3f6b395af9fc77
danidrd <danielecaliandro.dev@gmail.com>
2025-01-04 22:14:43 +0100

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Contacts from "./pages/Contact";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
   );
 }
